b39a71cb68a61ffedfbddc9295b63a6ec0184bfd
Durga Anil <durgaanilanukula64@gmail.com>
2025-02-23 22:42:37 +0530

    error fix

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
